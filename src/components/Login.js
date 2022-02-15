7ee84477fa8590a8a987a401daa8ffd265650812
K.Matsuo <k.matsuo@cybermissions.co.jp>
2022-02-15 16:00:51 +0900

    signin

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,52 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+
 const Login = () => {
-  return <h1>ログイン画面</h1>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    signInWithEmailAndPassword(auth, email, password);
+  };
+  const handleChangeEmail = (event) => {
+    setEmail(event.currentTarget.value);
+  };
+  const handleChangePassword = (event) => {
+    setPassword(event.currentTarget.value);
+  };
+
+  return (
+    <div>
+      <h1>ログイン</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>メールアドレス</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="email"
+            onChange={handleChangeEmail}
+          />
+        </div>
+        <div>
+          <label>パスワード</label>
+          <input
+            type="password"
+            name="password"
+            onChange={handleChangePassword}
+          />
+        </div>
+        <div>
+          <button>ログイン</button>
+        </div>
+        <div>
+          ユーザ登録は <Link to="/signup">こちら</Link>から
+        </div>
+      </form>
+    </div>
+  );
 };
 export default Login;
