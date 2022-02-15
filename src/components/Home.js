24ab248d5be4a05915e54424502d671435060dd3
K.Matsuo <k.matsuo@cybermissions.co.jp>
2022-02-15 16:09:05 +0900

    logout

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,19 @@
+import { signOut } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../firebase";
+
 const Home = () => {
-  return <h1>ホームページ</h1>;
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    signOut(auth);
+    navigate("/login");
+  };
+
+  return (
+    <div>
+      <h1>ホームページ</h1>
+      <button onClick={handleLogout}>ログアウト</button>
+    </div>
+  );
 };
 export default Home;
